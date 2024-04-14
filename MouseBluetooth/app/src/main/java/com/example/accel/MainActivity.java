50eab7195c372b0576d238689a517227b013b2db
damg22 <moral113@purdue.edu>
2024-04-14 13:46:58 -0700

    attempting to fix dt^2 and removing ignoring events

--- MouseBluetooth/app/src/main/java/com/example/accel/MainActivity.java
+++ MouseBluetooth/app/src/main/java/com/example/accel/MainActivity.java
@@ -388,19 +388,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
 
         private double getAxisDistance2(float acceleration, int MASK, long dt) {
-            if(ignoreEvents[MASK] > 0) {
-                ignoreEvents[MASK]--;
-                previousAcceleration[MASK] = acceleration;
-                return 0;
-            }
 
             if (THRESHOLD > Math.abs(acceleration)) {
                 previousAcceleration[MASK] = 0;
                 return 0;
             }
 
-            if(!(previousAcceleration[MASK] < 0 && acceleration < 0) ^ !(previousAcceleration[MASK] > 0 && acceleration>0)){
-                ignoreEvents[MASK] = NUM_IGNORE-1;
+            if(!(previousAcceleration[MASK] <= 0 && acceleration < 0) ^ !(previousAcceleration[MASK] >= 0 && acceleration>0)) {
                 previousAcceleration[MASK] = 0;
                 return 0;
             }
@@ -408,7 +402,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             double avgAcceleration = (previousAcceleration[MASK] + acceleration)/2.0;
             previousAcceleration[MASK] = acceleration;
 
-            return 1.0/6.0 * avgAcceleration*avgAcceleration*avgAcceleration * dt/1000000000.0;
+            return 1.0/6.0 * avgAcceleration*avgAcceleration*avgAcceleration * (dt/1000000000.0 * dt/1000000000.0);
         }
 
     }
